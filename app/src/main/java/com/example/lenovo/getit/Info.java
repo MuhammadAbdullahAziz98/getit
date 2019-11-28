0d20e3de24f8a5f89e0c71b7a51f1f088b1f5abf
abdullah <abdullahaziz1998@gmail.com>
2019-11-28 19:26:16 +0500

    Update of the year

--- app/src/main/java/com/example/lenovo/getit/Info.java
+++
@@ -1,9 +0,0 @@
-package com.example.lenovo.getit;
-
-class Info {
-    String mEmail,tMail;
-    public Info(String mEmail, String tMail) {
-        this.mEmail = mEmail;
-        this.tMail = tMail;
-    }
-}
--- app/src/main/java/com/example/lenovo/getit/ViewHolder.java
+++
@@ -1,8 +0,0 @@
-package com.example.lenovo.getit;
-
-import android.widget.TextView;
-
-public class ViewHolder {
-    TextView description;
-
-}
